ef25ff134067cf8205f02c48927664a636654bdc
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2021-06-29 18:53:12 +0900

    Update smoother and calc E

--- high_precision_guide_controller_sim.py
+++ high_precision_guide_controller_sim.py
@@ -49,34 +49,22 @@ class WaypointTracer():
         v_pre = self.u_pre[0]
         w_pre = self.u_pre[1]
         if abs(v-v_pre) > v_acc_max * self.dt:
-            print("v_acc_limitation!        v: %2.4f, vp: %2.4f, w: %2.4f, wp: %2.4f" %(v, v_pre, w, w_pre))
             if v > v_pre:
-                v_new = v_pre + v_acc_max * self.dt
+                v = v_pre + v_acc_max * self.dt
             else:
-                v_new = v_pre - v_acc_max * self.dt
-            if v != 0.0:
-                w = w * abs(v_new / v)
-            v = v_new
-            v, w = self.limit_input(np.array([v, w]))
-            print("v_acc_limitation result! v: %2.4f, vp: %2.4f, w: %2.4f, wp: %2.4f" %(v, v_pre, w, w_pre))
+                v = v_pre - v_acc_max * self.dt
         if abs(w-w_pre) > w_acc_max * self.dt:
-            print("w_acc_limitation!        v: %2.4f, vp: %2.4f, w: %2.4f, wp: %2.4f" %(v, v_pre, w, w_pre))
             if w > w_pre:
-                w_new = w_pre + w_acc_max * self.dt
+                w = w_pre + w_acc_max * self.dt
             else:
-                w_new = w_pre - w_acc_max * self.dt
-            if w != 0.0:
-                v = v * abs(w_new / w)
-            w = w_new
-            v, w = self.limit_input(np.array([v, w]))
-            print("w_acc_limitation result! v: %2.4f, vp: %2.4f, w: %2.4f, wp: %2.4f" %(v, v_pre, w, w_pre))
+                w = w_pre - w_acc_max * self.dt
         self.u_pre = np.array([v, w])
         return self.u_pre
 
 class WaypointTracerEx(WaypointTracer):
     def calc_u(self, X: np.ndarray) -> np.ndarray:
         t = X[2]
-        Xcp = X[:2] + np.array([np.cos(t), np.sin(t)]) * self.d / 1.5 * 0
+        Xcp = X[:2] + np.array([np.cos(t), np.sin(t)]) * self.d / 2.1
         u = - 0.2 * np.dot(np.linalg.inv(self.calc_B(t)), np.dot(self.K, Xcp))
         u = self.limit_input(u)
         u = self.limit_acceleration(u)
@@ -89,9 +77,9 @@ class WaypointTracerSwitch(WaypointTracerEx):
 
     def calc_u(self, X: np.ndarray) -> np.ndarray:
         t = X[2]
-        Xcp = X[:2] + np.array([np.cos(t), np.sin(t)]) * self.d / 1.5
+        Xcp = X[:2] + np.array([np.cos(t), np.sin(t)]) * self.d / 2.1
         if self.mode == 0:
-            if (np.linalg.norm(Xcp) > 0.1 or self.check_finish(X)) and X[0] > abs(X[1]):
+            if (np.linalg.norm(Xcp) > 0.1 or self.check_finish(X)) and (X[0] > abs(X[1]) or X[0] > 1.0):
                 return super().calc_u(X)
             else:
                 print("Reverse mode start!")
@@ -185,12 +173,14 @@ def check_finish(X):
 
 if __name__ == '__main__':
     dt = 0.1
-    T = 40.0
+    T = 60.0
     t = np.arange(0.0, T, dt)
     t_num = len(t)
-    controller = WaypointTracerEx(dt)
-    X0 = np.array([1.0, 1.0, 0.0])
+    controller = WaypointTracerSwitch(dt)
+    X0 = np.array([1.50, 1.40, 0.0])
+    Xd = np.array([1.0, -1.0, np.pi/4])
     X = np.zeros((3, t_num))
+    E = np.zeros((3, t_num))
     X[:,0] = X0
     U = np.zeros((2, t_num))
     for i in range(t_num-1):
@@ -198,10 +188,21 @@ if __name__ == '__main__':
         if theta > np.pi:
             theta -= 2*np.pi
         X[2,i] = theta
-        U[:,i] = controller.calc_u(X[:,i]+rand(n=3,s=0.01)*0)
+        e = X[:,i] - Xd
+        e[:2] = rotate(e[:2], -Xd[2])
+        E[:,i] = e
+        U[:,i] = controller.calc_u(e+rand(n=3,s=0.01)*0)
         X[:,i+1] = update_X(X[:,i], U[:,i]+rand(s=0.005)*0, dt)
+    theta = X[2,-1] % (2*np.pi)
+    if theta > np.pi:
+        theta -= 2*np.pi
+    X[2,i] = theta
+    e = np.zeros(3)
+    e[:2] = rotate(X[:2,-1] - Xd[:2], -Xd[2])
+    e[2] = X[2,-1] - Xd[2]
+    E[:,-1] = e
     theta = X[2,:]
-    Xcp = X[:2,:] + np.array([np.cos(theta), np.sin(theta)]) * -0.15 / 1.5
+    Xcp = X[:2,:] + np.array([np.cos(theta), np.sin(theta)]) * -0.15 / 2.1
     
     plt.close('all')
     tb_rect = np.array([[-0.2, 0.2, 0.2, -0.2, -0.2],
@@ -217,7 +218,7 @@ if __name__ == '__main__':
             area = plt.plot(area_rect[0,:], area_rect[1,:], color='w')
             tb = plt.fill(tb_rect_a[0,:]+X[0,i], tb_rect_a[1,:]+X[1,i], color='b')
             traj = plt.plot(Xcp[0,:i], Xcp[1,:i], color='k')
-            goal = plt.plot(0.0, 0.0, marker='*', color='k')
+            goal = plt.plot(Xd[0], Xd[1], marker='*', color='k')
             arr = plt.arrow(X[0,i], X[1,i], 0.4*np.cos(X[2,i]), 0.4*np.sin(X[2,i]), width=0.01,head_width=0.08,head_length=0.12, color='r')
             arrs = [arr]
             ims.append(area+tb+traj+goal+arrs)
@@ -226,13 +227,13 @@ if __name__ == '__main__':
     plt.show(block=False)
     # ani.save("output.gif", writer="imagemagick")
     print("Simulation finished!")
-    print("x_error: %2.4f [m], y_error: %2.4f [m], t_error: %2.4f [deg]" % (X[0,-1], X[1,-1], X[2,-1]*180/np.pi))
+    print("x_error: %2.4f [m], y_error: %2.4f [m], t_error: %2.4f [deg]" % (E[0,-1], E[1,-1], E[2,-1]*180/np.pi))
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111)
     ax2.plot(t,U[0,:], label='v')
     ax2.plot(t,U[1,:], label='w')
-    ax2.plot(t,X[1,:], label='y')
-    ax2.plot(t,X[2,:], label='theta')
+    ax2.plot(t,E[1,:], label='e_y')
+    ax2.plot(t,E[2,:], label='e_theta')
     plt.legend()
     plt.show(block=False)
     # sx = np.dot(controller.S, X)
