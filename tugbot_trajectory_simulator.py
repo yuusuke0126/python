17a8b5a448db8ee0a745fa14c30f1fc8f79c16c9
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2023-04-14 14:27:53 +0900

    Add simulator

---
+++ tugbot_trajectory_simulator.py
@@ -0,0 +1,228 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as patches
+import time, yaml
+
+from math import sin, cos, asin, acos, pi, sqrt, tan
+# from scipy.integrate import odeint
+
+def calc_cross_point(pointA, pointB, pointC, pointD):
+  cross_point = (0,0)
+  bunbo = (pointB[0] - pointA[0]) * (pointD[1] - pointC[1]) - (pointB[1] - pointA[1]) * (pointD[0] - pointC[0])
+  # Case in lines are parallel
+  if (bunbo == 0):
+      return False, cross_point
+  vectorAC = ((pointC[0] - pointA[0]), (pointC[1] - pointA[1]))
+  r = ((pointD[1] - pointC[1]) * vectorAC[0] - (pointD[0] - pointC[0]) * vectorAC[1]) / bunbo
+  distance = ((pointB[0] - pointA[0]) * r, (pointB[1] - pointA[1]) * r)
+  cross_point = np.array([pointA[0] + distance[0], pointA[1] + distance[1]])
+  return True, cross_point
+
+def rotate(v, theta=-np.pi/2.0):
+  rotateMat = np.array([
+      [np.cos(theta), -np.sin(theta)],
+      [np.sin(theta), np.cos(theta)],
+  ])
+  return np.dot(rotateMat, v)
+
+def update_status(t, z, p, theta, phi, v=0.5, w=0.0):
+  xd = v*cos(theta[t])
+  yd = v*sin(theta[t])
+  thetad = w
+  pxd = v*cos(theta[t]) - (-p_dist)*w*sin(theta[t])
+  pyd = v*sin(theta[t]) + (-p_dist)*w*cos(theta[t])
+  phid = (-pxd * sin(phi[t]) + pyd * cos(phi[t])) / c_dist
+  z[0, t+1] = z[0, t] + xd * dt
+  z[1, t+1] = z[1, t] + yd * dt
+  theta[t+1] = theta[t] + thetad * dt
+  p[0, t+1] = p[0, t] + pxd * dt
+  p[1, t+1] = p[1, t] + pyd * dt
+  phi[t+1] = phi[t] + phid * dt
+
+SAVE_FLAG = False
+plt.close('all')
+with open('config/uturn_with_rear_free.yaml', 'r') as yml:
+  config = yaml.safe_load(yml)
+
+p_dist = config['p_dist']  # distance between tugbot wheel center and pivot point of gripper
+c_dist = config['c_dist']  # distance between pivot point of gripper and cargo wheel center
+
+cf_dist = config['cf_dist']  # distance between pivot point of gripper and cart front edge
+cart_length = config['cart_length']  # for cargo cart
+cart_width = config['cart_width']  # for cargo cart
+
+cr_dist = -(cf_dist + cart_length)  # distance between pivot point of gripper and cart rear edge
+cw_2 = cart_width / 2.0
+
+max_angle = 60.0*pi/180.0
+a = pi/2.0 - max_angle
+
+motion_plan = config['motion_plan']
+
+x0 = config['x0']  # initial x of tugbot wheel center
+y0 = config['y0']  # initial y of tugbot wheel center
+
+init_theta = config['init_theta'] * pi / 180 # initial tugbot angle
+init_phi = config['init_phi'] * pi / 180  # initial cargo angle
+
+z = np.zeros((2,1))
+p = np.zeros((2,1))
+theta = np.zeros(1)
+phi = np.zeros(1)
+
+z[0,0], z[1,0], theta[0], phi[0] = x0, y0, init_theta, init_phi
+p[0,0] = z[0,0] + (-p_dist) * cos(theta[0])
+p[1,0] = z[1,0] + (-p_dist) * sin(theta[0])
+
+dt = 0.002
+for i in range(len(motion_plan)):
+  motion_type = motion_plan[i]["type"]
+  goal = motion_plan[i]["goal"]
+  if motion_type == "linear":
+    v = np.sign(goal) * 0.5
+    w = 0.0
+    step_num = np.ceil(abs(goal / (v*dt))).astype(int)
+  elif motion_type == "curve":
+    goal = goal * pi / 180
+    r = motion_plan[i]["curve_radius"]
+    v = np.sign(r) * 0.5
+    w = np.sign(goal) * abs(v / r)
+    step_num = np.ceil(abs(goal / (w*dt))).astype(int)
+  elif motion_type == "spin":
+    goal = goal * pi / 180
+    v = 0.0
+    w = np.sign(goal) * abs(motion_plan[i]["curve_radius"])
+    step_num = np.ceil(abs(goal / (w*dt))).astype(int)
+  current_t = z.shape[1]
+  z = np.hstack((z, np.zeros((2, step_num))))
+  p = np.hstack((p, np.zeros((2, step_num))))
+  theta = np.hstack((theta, np.zeros(step_num)))
+  phi = np.hstack((phi, np.zeros(step_num)))
+  for t in range(current_t-1, current_t+step_num-1):
+    update_status(t, z, p, theta, phi, v, w)
+  if motion_type == "linear":
+    dist = np.linalg.norm(z[:,current_t] - z[:,-2])
+    v = (abs(goal) - dist) / dt * np.sign(goal)
+    w = 0.0
+  elif motion_plan == "curve" or motion_plan == "spin":
+    dist_theta = theta[-2] - theta[current_t]
+    rest = goal - dist_theta
+    rest = rest % (2*pi)
+    if rest > pi:
+      rest -= 2*pi
+    w = rest / dt
+    if motion_plan == "curve":
+      v = (r * w) * np.sign(goal)
+    else:
+      v = 0.0
+  t = current_t + step_num - 1
+  update_status(t-1, z, p, theta, phi, v, w)
+
+fig = plt.figure()
+ims = []
+area_rect = np.loadtxt('config/areas/nisshin.csv', delimiter=',')
+# print(area_rect)
+kc_rect = np.array([[-0.27,  0.27, 0.27, -0.27, -0.27],
+                    [-0.30, -0.30, 0.30,  0.30, -0.30]])
+# cargo_rect = np.array([[-1.90, -0.90, -0.90, -1.90, -1.90, -2.125, -2.125, -2.413, -2.413, -2.125, -2.125,  -1.90, -1.90],
+#                        [-0.20, -0.20,  0.20,  0.20, 0.125,  0.125,  0.060,  0.060, -0.060, -0.060, -0.125, -0.125, -0.20]])  # case for nishsin seifun cart
+cargo_rect = np.array([[cr_dist, -cf_dist, -cf_dist, cr_dist, cr_dist],
+                       [  -cw_2,    -cw_2,     cw_2,    cw_2,   -cw_2]])  # general purpose
+rack_rect = np.array([[2.125, 5.875, 5.875, 2.125, 2.125],
+                      [  1.9,   1.9,   3.1,   3.1,   1.9]])
+box1_rect = np.array([[7.55, 8.00, 8.00, 7.55, 7.55],
+                      [4.55, 4.55, 5.00, 5.00, 4.55]])
+box2_rect = np.array([[7.55, 8.00, 8.00, 7.55, 7.55],
+                      [0.00, 0.00, 0.45, 0.45, 0.00]])
+zed_pose = np.array([[0.55], 
+                     [0.02]])
+connect_line = np.array([[0, -p_dist], 
+                         [0, 0]])
+pivot_line = np.array([[0, -c_dist], 
+                         [0, 0]])
+
+cargo_rear_right = np.zeros(z.shape)
+cargo_center_right = np.zeros(z.shape)
+kc_front_left = np.zeros(z.shape)
+dist = np.zeros(z.shape[1])
+zed_dist1 = np.zeros(z.shape[1])
+x_min = 0.0
+x_max = 0.0
+y_min = 0.0
+y_max = 0.0
+
+for i in range(z.shape[1]):
+  kc_rect_angle = np.zeros([2,5])
+  cargo_rect_angle = np.zeros([2,5])
+  kc_rect_angle = rotate(kc_rect, theta[i])
+  cargo_rect_angle = rotate(cargo_rect, phi[i])
+  connect_angle = rotate(connect_line, theta[i])
+  pivot_angle = rotate(pivot_line, phi[i])
+  zed_position = z[:,i:i+1] + rotate(zed_pose, theta[i])
+  zed_fov1 = zed_position + rotate(np.array([[1.0],[0.0]]), theta[i]+np.arctan(0.8/1.5))
+  # zed_dists = []
+  # for j in range(3):
+  #   flag, fov = calc_cross_point(zed_position,zed_fov1,area_rect[:,j],area_rect[:,j+1])
+  #   if flag:
+  #     zed_dists.append(np.linalg.norm(zed_position-fov))
+  # # flag1, fov11 = calc_cross_point(zed_position,zed_fov1,area_rect[:,1],area_rect[:,2])
+  # # flag2, fov12 = calc_cross_point(zed_position,zed_fov1,area_rect[:,2],area_rect[:,3])
+  # # flag3, fov13 = calc_cross_point(zed_position,zed_fov1,area_rect[:,0],area_rect[:,1])
+
+  # zed_dist1[i] = min(zed_dists)
+  cargo_rear_right[:,i] = cargo_rect_angle[:,0] + z[:,i]  # np.array([cargo_rect_angle[0,0]+z[0,i], cargo_rect_angle[1,0]+z[1,i]])
+  cargo_center_right[:,i] = (cargo_rect_angle[:,0] + cargo_rect_angle[:,1])/2.0 + z[:,i]
+  kc_front_left[:,i] = kc_rect_angle[:,2] + z[:,i]  # np.array([kc_rect_angle[0,2]+z[0,i], kc_rect_angle[1,2]+z[1,i]])
+  dist[i] = np.linalg.norm(cargo_rear_right[:,i] - area_rect[:,1])
+  x_min_i = min(min(kc_rect_angle[0]) + z[0,i], min(cargo_rect_angle[0]) + p[0,i])
+  x_max_i = max(max(kc_rect_angle[0]) + z[0,i], max(cargo_rect_angle[0]) + p[0,i])
+  y_min_i = min(min(kc_rect_angle[1]) + z[1,i], min(cargo_rect_angle[1]) + p[1,i])
+  y_max_i = max(max(kc_rect_angle[1]) + z[1,i], max(cargo_rect_angle[1]) + p[1,i])
+  x_min = min(x_min, x_min_i)
+  x_max = max(x_max, x_max_i)
+  y_min = min(y_min, y_min_i)
+  y_max = max(y_max, y_max_i)
+  x_array = [x_min, x_max, x_max, x_min, x_min]
+  y_array = [y_min, y_min, y_max, y_max, y_min]
+  if i % int(round(0.1/dt)) == 0:
+    kc = plt.fill(kc_rect_angle[0]+z[0,i], kc_rect_angle[1]+z[1,i], color='b')
+    cargo = plt.fill(cargo_rect_angle[0]+p[0,i], cargo_rect_angle[1]+p[1,i], color='r')
+    connect = plt.plot(connect_angle[0]+z[0,i], connect_angle[1]+z[1,i], linewidth=4.0, color='k')
+    pivot = plt.plot(pivot_angle[0]+p[0,i], pivot_angle[1]+p[1,i], linewidth=4.0, color='k')
+    # area = plt.plot(area_rect[0], area_rect[1], color='m')
+    area = plt.plot(x_array, y_array, color='m')
+    # for k in reversed(range(10)):
+    #   l = (i - i%int(round(0.1/dt)*5)) + k*int(round(0.1/dt))*5
+    #   if l > x.shape[0] - 1:
+    #     l = x.shape[0] - 1
+    #   r_v = sqrt(xd[l]**2+yd[l]**2)
+    #   if r_v < 0.1:
+    #     r_v = 0.1
+    #   if k==0:
+    #     l = i
+    #     r_v = sqrt(xd[l]**2+yd[l]**2)
+    #     if r_v < 0.1:
+    #       r_v = 0.1
+    #     arr = plt.arrow(x[l], y[l], r_v*cos(theta[l]), r_v*sin(theta[l]), width=0.01,head_width=0.08,head_length=0.12, color='r')
+    #   else:
+    #     arr = plt.arrow(x[l], y[l], r_v*cos(theta[l]), r_v*sin(theta[l]), width=0.005,head_width=0.08,head_length=0.12, color='k')
+    #   arrs.append(arr)
+    ims.append(kc+cargo+connect+pivot+area)
+
+print("minimum distance between cargo to inside rack: {:.2f} [m]".format(min(dist)))
+# print("minimum distance between tugbot to wall: {:.2f} [m]".format(min(8.0-max(kc_front_left[0,:]),min(kc_front_left[1,:]))))
+# print("minimum distance between tugbot to box: {:.2f} [m]".format(min(np.array([np.linalg.norm(kc_front_left[:,i]-np.array([7.55, 0.45])) for i in range(x.shape[0])]))))
+# print("minimum distance between cargo to wall: {:.2f} [m]".format(kc2wall-0.33))
+# print("stop point: {:.2f} [m]".format(stop_dist - 0.71))
+print("curve radius: %1.2f [m]" % (r))
+print("x_dist: %1.2f [m], y_dist: %1.2f [m]" % (x_max - x_min, y_max - y_min))
+print("max angle: %1.2f [deg]" % (max(abs(theta-phi)*180/pi)))
+ani = animation.ArtistAnimation(fig, ims, interval=0.1*1000/2, repeat=False)
+plt.axis('equal')
+plt.show(block=True)
+if SAVE_FLAG:
+  val = input("Do you want to save the gif? Y/n: ")
+  if val == "Y" or val == "y":
+    print("Saving gif...")
+    ani.save("output.gif", writer="imagemagick")
