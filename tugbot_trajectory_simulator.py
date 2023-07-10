901eeca9e4da0acaaae8d51099da872770a93ded
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2023-07-10 17:09:14 +0900

    UPD

--- tugbot_trajectory_simulator.py
+++ tugbot_trajectory_simulator.py
@@ -3,28 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
 import time, yaml
+from test_module import segment_distance_segment, rotate
 
 from math import sin, cos, asin, acos, pi, sqrt, tan
-# from scipy.integrate import odeint
-
-def calc_cross_point(pointA, pointB, pointC, pointD):
-  cross_point = (0,0)
-  bunbo = (pointB[0] - pointA[0]) * (pointD[1] - pointC[1]) - (pointB[1] - pointA[1]) * (pointD[0] - pointC[0])
-  # Case in lines are parallel
-  if (bunbo == 0):
-      return False, cross_point
-  vectorAC = ((pointC[0] - pointA[0]), (pointC[1] - pointA[1]))
-  r = ((pointD[1] - pointC[1]) * vectorAC[0] - (pointD[0] - pointC[0]) * vectorAC[1]) / bunbo
-  distance = ((pointB[0] - pointA[0]) * r, (pointB[1] - pointA[1]) * r)
-  cross_point = np.array([pointA[0] + distance[0], pointA[1] + distance[1]])
-  return True, cross_point
-
-def rotate(v, theta=-np.pi/2.0):
-  rotateMat = np.array([
-      [np.cos(theta), -np.sin(theta)],
-      [np.sin(theta), np.cos(theta)],
-  ])
-  return np.dot(rotateMat, v)
 
 def update_status(t, z, p, theta, phi, v=0.5, w=0.0):
   xd = v*cos(theta[t])
@@ -40,9 +21,12 @@ def update_status(t, z, p, theta, phi, v=0.5, w=0.0):
   p[1, t+1] = p[1, t] + pyd * dt
   phi[t+1] = phi[t] + phid * dt
 
-SAVE_FLAG = False
+config_file_name = "ebina_146x180_stab"
+area_file_name = "146x180"
+print(config_file_name + "\n" + area_file_name)
+SAVE_FLAG = True
 plt.close('all')
-with open('config/uturn_with_rear_free.yaml', 'r') as yml:
+with open('config/' + config_file_name + '.yaml', 'r') as yml:
   config = yaml.safe_load(yml)
 
 p_dist = config['p_dist']  # distance between tugbot wheel center and pivot point of gripper
@@ -92,7 +76,7 @@ for i in range(len(motion_plan)):
   elif motion_type == "spin":
     goal = goal * pi / 180
     v = 0.0
-    w = np.sign(goal) * abs(motion_plan[i]["curve_radius"])
+    w = np.sign(goal) * 1.0
     step_num = np.ceil(abs(goal / (w*dt))).astype(int)
   current_t = z.shape[1]
   z = np.hstack((z, np.zeros((2, step_num))))
@@ -119,22 +103,23 @@ for i in range(len(motion_plan)):
   t = current_t + step_num - 1
   update_status(t-1, z, p, theta, phi, v, w)
 
-fig = plt.figure()
+fig, ax = plt.subplots()
 ims = []
-area_rect = np.loadtxt('config/areas/nisshin.csv', delimiter=',')
+with open('config/areas/' + area_file_name + '.csv') as file:
+  area_num = int(sum(1 for line in file) / 2)
+area_rect_list = []
+for i in range(area_num):
+  area_rect = np.loadtxt('config/areas/' + area_file_name + '.csv', delimiter=',', skiprows=2*i, max_rows=2)
+  area_rect_list.append(area_rect)
 # print(area_rect)
 kc_rect = np.array([[-0.27,  0.27, 0.27, -0.27, -0.27],
-                    [-0.30, -0.30, 0.30,  0.30, -0.30]])
+                    [-0.30, -0.30, 0.30,  0.30, -0.30]])  # tugbot
+# kc_rect = np.array([[-0.09,  0.61, 0.61, -0.09, -0.09],
+#                     [-0.25, -0.25, 0.25,  0.25, -0.25]])  # keycart
 # cargo_rect = np.array([[-1.90, -0.90, -0.90, -1.90, -1.90, -2.125, -2.125, -2.413, -2.413, -2.125, -2.125,  -1.90, -1.90],
 #                        [-0.20, -0.20,  0.20,  0.20, 0.125,  0.125,  0.060,  0.060, -0.060, -0.060, -0.125, -0.125, -0.20]])  # case for nishsin seifun cart
 cargo_rect = np.array([[cr_dist, -cf_dist, -cf_dist, cr_dist, cr_dist],
                        [  -cw_2,    -cw_2,     cw_2,    cw_2,   -cw_2]])  # general purpose
-rack_rect = np.array([[2.125, 5.875, 5.875, 2.125, 2.125],
-                      [  1.9,   1.9,   3.1,   3.1,   1.9]])
-box1_rect = np.array([[7.55, 8.00, 8.00, 7.55, 7.55],
-                      [4.55, 4.55, 5.00, 5.00, 4.55]])
-box2_rect = np.array([[7.55, 8.00, 8.00, 7.55, 7.55],
-                      [0.00, 0.00, 0.45, 0.45, 0.00]])
 zed_pose = np.array([[0.55], 
                      [0.02]])
 connect_line = np.array([[0, -p_dist], 
@@ -151,6 +136,8 @@ x_min = 0.0
 x_max = 0.0
 y_min = 0.0
 y_max = 0.0
+dist_min = -1
+t_min = None
 
 for i in range(z.shape[1]):
   kc_rect_angle = np.zeros([2,5])
@@ -161,20 +148,31 @@ for i in range(z.shape[1]):
   pivot_angle = rotate(pivot_line, phi[i])
   zed_position = z[:,i:i+1] + rotate(zed_pose, theta[i])
   zed_fov1 = zed_position + rotate(np.array([[1.0],[0.0]]), theta[i]+np.arctan(0.8/1.5))
-  # zed_dists = []
-  # for j in range(3):
-  #   flag, fov = calc_cross_point(zed_position,zed_fov1,area_rect[:,j],area_rect[:,j+1])
-  #   if flag:
-  #     zed_dists.append(np.linalg.norm(zed_position-fov))
-  # # flag1, fov11 = calc_cross_point(zed_position,zed_fov1,area_rect[:,1],area_rect[:,2])
-  # # flag2, fov12 = calc_cross_point(zed_position,zed_fov1,area_rect[:,2],area_rect[:,3])
-  # # flag3, fov13 = calc_cross_point(zed_position,zed_fov1,area_rect[:,0],area_rect[:,1])
-
-  # zed_dist1[i] = min(zed_dists)
-  cargo_rear_right[:,i] = cargo_rect_angle[:,0] + z[:,i]  # np.array([cargo_rect_angle[0,0]+z[0,i], cargo_rect_angle[1,0]+z[1,i]])
-  cargo_center_right[:,i] = (cargo_rect_angle[:,0] + cargo_rect_angle[:,1])/2.0 + z[:,i]
-  kc_front_left[:,i] = kc_rect_angle[:,2] + z[:,i]  # np.array([kc_rect_angle[0,2]+z[0,i], kc_rect_angle[1,2]+z[1,i]])
-  dist[i] = np.linalg.norm(cargo_rear_right[:,i] - area_rect[:,1])
+  min_dist = None
+  min_point_robot = None
+  min_point_area = None
+  kc_z = kc_rect_angle + z[:,i:i+1]
+  cg_z = cargo_rect_angle + z[:,i:i+1]
+  for j in range(area_num):
+    area_rect = area_rect_list[j]
+    edge_num = area_rect.shape[1] - 1
+    for k in range(edge_num):
+      for l in range(4):
+        distance, pr, pa = segment_distance_segment(kc_z[:,l], kc_z[:,l+1], area_rect[:,k], area_rect[:,k+1])
+        if min_dist is None or distance < min_dist:
+          min_dist = distance
+          min_point_robot = pr
+          min_point_area = pa
+      for l in range(4):
+        distance, pr, pa = segment_distance_segment(cg_z[:,l], cg_z[:,l+1], area_rect[:,k], area_rect[:,k+1])
+        if min_dist is None or distance < min_dist:
+          min_dist = distance
+          min_point_robot = pr
+          min_point_area = pa
+  dist[i] = min_dist
+  if dist_min > min_dist or t_min is None:
+    dist_min = min_dist
+    t_min = i
   x_min_i = min(min(kc_rect_angle[0]) + z[0,i], min(cargo_rect_angle[0]) + p[0,i])
   x_max_i = max(max(kc_rect_angle[0]) + z[0,i], max(cargo_rect_angle[0]) + p[0,i])
   y_min_i = min(min(kc_rect_angle[1]) + z[1,i], min(cargo_rect_angle[1]) + p[1,i])
@@ -186,43 +184,45 @@ for i in range(z.shape[1]):
   x_array = [x_min, x_max, x_max, x_min, x_min]
   y_array = [y_min, y_min, y_max, y_max, y_min]
   if i % int(round(0.1/dt)) == 0:
-    kc = plt.fill(kc_rect_angle[0]+z[0,i], kc_rect_angle[1]+z[1,i], color='b')
-    cargo = plt.fill(cargo_rect_angle[0]+p[0,i], cargo_rect_angle[1]+p[1,i], color='r')
-    connect = plt.plot(connect_angle[0]+z[0,i], connect_angle[1]+z[1,i], linewidth=4.0, color='k')
-    pivot = plt.plot(pivot_angle[0]+p[0,i], pivot_angle[1]+p[1,i], linewidth=4.0, color='k')
-    # area = plt.plot(area_rect[0], area_rect[1], color='m')
-    area = plt.plot(x_array, y_array, color='m')
-    # for k in reversed(range(10)):
-    #   l = (i - i%int(round(0.1/dt)*5)) + k*int(round(0.1/dt))*5
-    #   if l > x.shape[0] - 1:
-    #     l = x.shape[0] - 1
-    #   r_v = sqrt(xd[l]**2+yd[l]**2)
-    #   if r_v < 0.1:
-    #     r_v = 0.1
-    #   if k==0:
-    #     l = i
-    #     r_v = sqrt(xd[l]**2+yd[l]**2)
-    #     if r_v < 0.1:
-    #       r_v = 0.1
-    #     arr = plt.arrow(x[l], y[l], r_v*cos(theta[l]), r_v*sin(theta[l]), width=0.01,head_width=0.08,head_length=0.12, color='r')
-    #   else:
-    #     arr = plt.arrow(x[l], y[l], r_v*cos(theta[l]), r_v*sin(theta[l]), width=0.005,head_width=0.08,head_length=0.12, color='k')
-    #   arrs.append(arr)
-    ims.append(kc+cargo+connect+pivot+area)
+    kc = ax.fill(kc_rect_angle[0]+z[0,i], kc_rect_angle[1]+z[1,i], color='b')
+    cargo = ax.fill(cargo_rect_angle[0]+p[0,i], cargo_rect_angle[1]+p[1,i], color='r')
+    connect = ax.plot(connect_angle[0]+z[0,i], connect_angle[1]+z[1,i], linewidth=4.0, color='k')
+    pivot = ax.plot(pivot_angle[0]+p[0,i], pivot_angle[1]+p[1,i], linewidth=4.0, color='k')
+    min_dist_line = ax.plot([min_point_robot[0], min_point_area[0]], [min_point_robot[1], min_point_area[1]], color='r')
+    areas = None
+    for j in range(area_num):
+      area = ax.plot(area_rect_list[j][0], area_rect_list[j][1], color='m')
+      areas = area if areas is None else areas + area
+    im = kc+cargo+connect+pivot+areas+min_dist_line
+    ims.append(im)
 
-print("minimum distance between cargo to inside rack: {:.2f} [m]".format(min(dist)))
-# print("minimum distance between tugbot to wall: {:.2f} [m]".format(min(8.0-max(kc_front_left[0,:]),min(kc_front_left[1,:]))))
-# print("minimum distance between tugbot to box: {:.2f} [m]".format(min(np.array([np.linalg.norm(kc_front_left[:,i]-np.array([7.55, 0.45])) for i in range(x.shape[0])]))))
-# print("minimum distance between cargo to wall: {:.2f} [m]".format(kc2wall-0.33))
-# print("stop point: {:.2f} [m]".format(stop_dist - 0.71))
-print("curve radius: %1.2f [m]" % (r))
+t_min *= dt
+stop_t = len(dist) * dt
+T = np.linspace(0.0, stop_t, len(dist))
+dist = np.sqrt(dist)
+dist_min = np.sqrt(dist_min)
+
+print("minimum distance between robot/cargo to wall: {:.2f} [m]".format(dist_min))
 print("x_dist: %1.2f [m], y_dist: %1.2f [m]" % (x_max - x_min, y_max - y_min))
 print("max angle: %1.2f [deg]" % (max(abs(theta-phi)*180/pi)))
 ani = animation.ArtistAnimation(fig, ims, interval=0.1*1000/2, repeat=False)
 plt.axis('equal')
 plt.show(block=True)
+plt.plot(T, dist, label="minimum distance")
+plt.plot([0,stop_t],[dist_min, dist_min], linestyle="--", label="%2.3f [m]" % (dist_min))
+plt.plot([t_min, t_min], [0,max(dist)], linestyle="--", label="%2.1f [sec]" % (t_min))
+plt.xlabel("t [sec]")
+plt.ylabel("minimum distance [m]")
+plt.legend()
+plt.show()
+
 if SAVE_FLAG:
   val = input("Do you want to save the gif? Y/n: ")
   if val == "Y" or val == "y":
+    val = input("File name: ")
+    if val == "":
+      val = "output"
+    file_name = val + ".gif"
+    print("filename: " + file_name)
     print("Saving gif...")
-    ani.save("output.gif", writer="imagemagick")
+    ani.save("gifs/" + file_name, writer="imagemagick")
